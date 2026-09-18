68be564b945b061a3920f1e601c87bffdd2c7898
lovecareglobal-cell <lovecareglobal@gmail.com>
2026-09-18 10:40:20 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,60 @@
+
+const CACHE_NAME = 'love-care-global-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './logo.png',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.22.0/firebase-database-compat.js'
+];
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installing');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(function(response) {
+          if(!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          var responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(function(cache) {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activating');
+  var cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
